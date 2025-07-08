5e64dacb926a4a6833fba77a5737f8a790999f30
Khasaleh <90069874+Khasaleh@users.noreply.github.com>
2025-07-08 00:56:34 -0400

    adding a fix 6

--- app/models/shopping_category.py
+++ app/models/shopping_category.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 from app.db.base_class import Base
 from app.db.schema_names import PUBLIC_SCHEMA
 from sqlalchemy.sql import func
+
 class ShoppingCategoryOrm(Base):
     __tablename__ = "shopping_categories"
     __table_args__ = (
@@ -10,14 +11,14 @@ class ShoppingCategoryOrm(Base):
         {"schema": PUBLIC_SCHEMA},
     )
 
-    id                  = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
-    name                = Column(String(150), nullable=False, index=True)
-    parent_id           = Column(BigInteger, ForeignKey(f"{PUBLIC_SCHEMA}.shopping_categories.id"), nullable=True)
+    id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(150), nullable=False, index=True)
+    parent_id = Column(BigInteger, ForeignKey(f"{PUBLIC_SCHEMA}.shopping_categories.id"), nullable=True)
     business_details_id = Column(BigInteger, ForeignKey(f"{PUBLIC_SCHEMA}.business_details.id"), nullable=False)
-    created_at          = Column(DateTime, server_default=func.now(), nullable=False)
-    updated_at          = Column(DateTime, onupdate=func.now(), nullable=True)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime, onupdate=func.now(), nullable=True)
 
-    # Relationship with BusinessDetailsOrm
+    # Relationship to BusinessDetails
     business_detail = relationship(
         "BusinessDetailsOrm",
         back_populates="shopping_categories"
@@ -37,7 +38,6 @@ class ShoppingCategoryOrm(Base):
         cascade="all, delete-orphan",
         single_parent=True
     )
-
 
 
     def __repr__(self):
